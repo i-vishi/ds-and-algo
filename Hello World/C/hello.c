29a1e9fa0187a4a877d3b89ee99fd6648be60089
Vishal Gaur <rr.vishalgaur@gmail.com>
2021-01-01 13:40:48 +0530

    added hello world programs

---
+++ Hello World/C/hello.c
@@ -0,0 +1,11 @@
+/* 	Author:		Vishal Gaur
+* 	Created:	01-01-2021  13:02:07
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World\n");
+  return 0;
+}
---
+++ Hello World/Cpp/hello.cpp
@@ -0,0 +1,11 @@
+/* 	Author:		Vishal Gaur
+* 	Created:	01-01-2021  12:59:37
+*/
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
